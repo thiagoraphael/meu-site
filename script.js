d1d1c3bc7ac4039c86be05ac8074ae8ee76e28e8
Thiago Raphael <thiagojustinianosilva@gmail.com>
2026-08-17 20:41:33 -0300

    Redesign: novo estilo editorial, seções Apps e Currículo, Instagram no lugar do X, blog reskinado

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
+const retangulos = document.querySelectorAll('.retangulo');
+const rotacaoBase = [-8, 5, -6, 7];
+let ticking = false;
+
 window.addEventListener('scroll', () => {
-    const retangulos = document.querySelectorAll('.retangulo');
-    const scrollY = window.scrollY;
-    
-    retangulos.forEach((ret, index) => {
-        const rotacaoBase = [-8, 5, -6, 7];
-        const novaRotacao = rotacaoBase[index] + (scrollY * 0.02);
-        ret.style.transform = `rotate(${novaRotacao}deg)`;
-    });
+    if (!ticking) {
+        requestAnimationFrame(() => {
+            const scrollY = window.scrollY;
+            retangulos.forEach((ret, index) => {
+                const novaRotacao = rotacaoBase[index] + (scrollY * 0.02);
+                ret.style.transform = `rotate(${novaRotacao}deg)`;
+            });
+            ticking = false;
+        });
+        ticking = true;
+    }
 });
